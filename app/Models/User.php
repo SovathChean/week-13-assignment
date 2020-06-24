2069640793a4161ef693da91df6237cdbda98058
SovathChean <sovathchean09gmail.com>
2020-06-24 13:36:58 +0700

    [TASK] install laravel_passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 use App\Models\Role;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
